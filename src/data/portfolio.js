d0839b6ddb5265cfab5f1b82802c3f305683f1d1
Mikey Abela <mabela@schmidtfutures.com>
2024-02-26 18:36:05 -0500

    remove pdf renderer

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,25 @@
 export default [
   {
-    title: 'US Food System',
-    imgUrl: '/assets/us-food.png',
-    tags: ['Data Visualization', 'Design', 'Web Development'],
-    link: '/projects/us-food',
+    title: "US Food System",
+    imgUrl: "/assets/us-food.png",
+    tags: [
+      "Data Visualization",
+      "Design",
+      "Web Development",
+      "Mapping",
+    ],
+    link: "/projects/us-food",
   },
   {
-    title: 'Earth Index',
-    imgUrl: '/assets/earth-index.png',
-    tags: ['Product Management', 'Mapping', 'Web Development'],
-    link: '/projects/earth-index',
+    title: "Earth Index",
+    imgUrl: "/assets/earth-index.png",
+    tags: ["Product Management", "Mapping", "Web Development"],
+    link: "/projects/earth-index",
   },
   {
-    title: 'Climate TRACE Data Insights',
-    imgUrl: '/assets/climate-trace.png',
-    tags: ['Data Visualization', 'Mapping'],
-    link: '/projects/climate-trace',
+    title: "Climate TRACE Data Insights",
+    imgUrl: "/assets/climate-trace.png",
+    tags: ["Data Visualization", "Mapping"],
+    link: "/projects/climate-trace",
   },
 ];
