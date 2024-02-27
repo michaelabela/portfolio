646209f1da6b6ef700f70d6f0ed396523a625ed9
Mikey Abela <mabela@schmidtfutures.com>
2024-02-26 23:04:24 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import Intro from './components/Intro';
-import Portfolio from './components/Portfolio';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import USFood from './components/projects/USFood';
-import EarthIndex from './components/projects/EarthIndex';
-import ClimateTRACE from './components/projects/ClimateTRACE';
+import React from "react";
+import Intro from "./components/Intro";
+import Portfolio from "./components/Portfolio";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import USFood from "./components/projects/USFood";
+import EarthIndex from "./components/projects/EarthIndex";
+import ClimateTRACE from "./components/projects/ClimateTRACE";
+import GothamWriters from "./components/projects/GothamWriters";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={
             <div>
               <Intro />
@@ -19,11 +20,15 @@ function App() {
             </div>
           }
         />
-        <Route path='/projects/us-food' element={<USFood />} />
-        <Route path='/projects/earth-index' element={<EarthIndex />} />
+        <Route path="/projects/us-food" element={<USFood />} />
+        <Route path="/projects/earth-index" element={<EarthIndex />} />
         <Route
-          path='/projects/climate-trace'
+          path="/projects/climate-trace"
           element={<ClimateTRACE />}
+        />
+        <Route
+          path="/projects/gotham-writers"
+          element={<GothamWriters />}
         />
       </Routes>
     </Router>
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -17,9 +17,63 @@ export default [
     link: "/projects/earth-index",
   },
   {
+    title: "Food Twin Pesentation",
+    imgUrl: "/assets/food-twin.png",
+    tags: ["Public Speaking"],
+    link: "https://youtu.be/2ysHfZ6Qqu4?t=75",
+  },
+  {
     title: "Climate TRACE Data Insights",
     imgUrl: "/assets/climate-trace.png",
     tags: ["Data Visualization", "Mapping"],
     link: "/projects/climate-trace",
+  },
+  {
+    title: "Gotham Writers Workshop",
+    imgUrl: "/assets/gotham-image.png",
+    tags: ["Writing", "Journalism"],
+    link: "/projects/gotham-writers",
+  },
+  {
+    title: "Meta-Metaverse",
+    imgUrl: "/assets/metaverse.jpeg",
+    tags: ["360 Video", "Graphics", "Design"],
+    link: "https://medium.com/@michael_abela/the-meta-meta-verse-7ce93666a9ce",
+  },
+  {
+    title: "HeartBeam",
+    imgUrl: "/assets/heartbeam.jpeg",
+    tags: ["360 Modeling", "3D Printing", "Design"],
+    link: "https://medium.com/@michael_abela/heartbeam-6818c148dc7b",
+  },
+  {
+    title: "Planet Generator",
+    imgUrl: "/assets/planet-generator.png",
+    tags: ["Software Development", "Graphics"],
+    link: "https://vimeo.com/916958058?share=copy",
+  },
+  {
+    title: "Node Trip",
+    imgUrl: "/assets/node-trip.jpg",
+    tags: ["Web Development", "Mapping"],
+    link: "https://vimeo.com/916959984?share=copy",
+  },
+  {
+    title: "Betweeen Light and Sound",
+    imgUrl: "/assets/light-and-sound.png",
+    tags: ["Video Editing"],
+    link: "https://vimeo.com/335723088",
+  },
+  {
+    title: "BMP Promotional Video",
+    imgUrl: "/assets/bmp.png",
+    tags: ["Video Editing"],
+    link: "https://vimeo.com/356273195",
+  },
+  {
+    title: "Just Bike",
+    imgUrl: "/assets/just-bike.png",
+    tags: ["Videography", "Video Editing"],
+    link: "https://vimeo.com/346738964?share=copy",
   },
 ];
