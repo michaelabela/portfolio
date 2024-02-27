ca197a34f5345b2baf0fd468f61414bfa9eaadd9
Mikey Abela <mabela@schmidtfutures.com>
2024-02-26 23:10:48 -0500

    add portfolio bottom padding

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import portfolio from '../data/portfolio';
-import PortfolioItem from './PortfolioItem';
+import React from "react";
+import portfolio from "../data/portfolio";
+import PortfolioItem from "./PortfolioItem";
 
 function Portfolio() {
   return (
-    <div className='flex flex-col md:flex-row items-center justify-center'>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+    <div className="flex flex-col md:flex-row items-center justify-center mb-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {portfolio.map((project, index) => {
           return (
             <PortfolioItem
